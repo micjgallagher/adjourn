bac8bc0bea0a93932595d701b8a63d841a08a085
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2025-05-14 23:03:31 -0400

    Add `confirm_action` helper method.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string.h>
 
 #include "deps/json.hpp"
 using json = nlohmann::json;
 
 
-int main (int argc, char *argv[]) {
+const char *cmd_write = "write";
+
+
+bool confirm_action(char *msg) {
+    std::cout << msg << "(y/n)\n> ";
+    char *in;
+    std::cin >> in;
+    return strcmp(in, "y");
+}
+
+
+
+int main (int argc, char **argv) {
     for (int i = 0; i < argc; i++) {
         std::cout << argv[i] << "\n";
     }
